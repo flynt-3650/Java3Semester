353ac5f49428b8ddf26aed66d9d579d90c52f9f3
fl_3650 <repilovi@gmail.com>
2023-12-09 23:12:34 +0300

    bin. search now functions properly

--- src/_test/Main.java
+++ src/_test/Main.java
@@ -9,12 +9,15 @@ class Main {
     private static int rec(int[] arr, int target, int left, int right) {
         int mid = (left + right) / 2;
 
+        if (left > right)
+            return -1;
+
         if (arr[mid] == target) {
             return mid;
         } else if (arr[mid] < target) {
-            return rec(arr, target, mid, right);
+            return rec(arr, target, mid + 1, right);
         } else {
-            return rec(arr, target, left, mid);
+            return rec(arr, target, left, mid - 1);
         }
     }
 
@@ -27,7 +30,6 @@ class Main {
     public static void main(String[] args) {
         int[] lst1 = new int[]{-1, 0, 3, 5, 9, 12};
         int[] lst2 = new int[]{-1, 0, 3, 5, 9, 12};
-
 
         System.out.println(binarySearch(lst1, 9));
         System.out.println(binarySearch(lst2, 2));
